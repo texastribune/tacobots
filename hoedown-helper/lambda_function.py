76c3284e6aef9513c6ab4e5eb017dd596a2af23d
Avriana <avrianaallen2021@u.northwestern.edu>
2020-08-20 17:30:36 -0500

    Can I post a threaded message?

--- hoedown-helper/lambda_function.py
+++ hoedown-helper/lambda_function.py
@@ -1,10 +1,22 @@
-def lambda_handler(event, context):
-    # print logs to aws for debugging / development
-    print(f"Received event:\n{event}\nWith context:\n{context}")
 
-    challenge_answer = event.get("challenge")
-    
-    return {
-        'statusCode': 200,
-        'body': challenge_answer
-    }
+def lambda_handler(data, context):
+    # print logs to aws for debugging / development
+    print(f"Received event:\n{data}\nWith context:\n{context}")
+
+    event = data['event']
+    thread = event['ts']
+    channel = event['channel']
+
+
+    challenge_answer = data.get("challenge")
+    if challenge_answer:
+        return {
+            'statusCode': 200,
+            'body': challenge_answer
+        }
+    else:
+        return {
+            "channel": channel,
+            "thread_ts": thread,
+            "text": "Responding in a thread, whoo!"
+        }
