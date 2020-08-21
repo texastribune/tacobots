4c3356b1b608bf17c93357dcfdf0bf6a947eb146
Avriana <avrianaallen2021@u.northwestern.edu>
2020-08-20 19:13:48 -0500

    Thread a message

--- hoedown-helper/lambda_function.py
+++ hoedown-helper/lambda_function.py
@@ -1,22 +1,42 @@
-
+import requests
+import os
 def lambda_handler(data, context):
     # print logs to aws for debugging / development
     print(f"Received event:\n{data}\nWith context:\n{context}")
-
+    # Set headers for requests
+    headers = {
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer xoxb-{}'.format(os.environ['BOT_TOKEN'])
+    }  
+    # Get event
     event = data['event']
-    thread = event['ts']
-    channel = event['channel']
-
-
+    # Check if the request contains a challenge
     challenge_answer = data.get("challenge")
+    # If it does, verify the request
     if challenge_answer:
         return {
             'statusCode': 200,
             'body': challenge_answer
         }
+
+    # Get Response Channel
+    channel = event['channel']
+    # If the message is a thread, get the text and thread ts.
+    if event["thread_ts"]:
+        thread = event['thread_ts']
+        message = event['blocks'][0]['elements'][0]['elements'][0]['text']
+    # Otherwise get the normal test and ts
     else:
-        return {
+        thread = event['ts']
+        message = event['text']
+    # Create response and set the thread and channel
+    reply_info = {
             "channel": channel,
             "thread_ts": thread,
-            "text": "Responding in a thread, whoo!"
-        }
+            "text": "You said{}".format(message)
+    }
+    # Format the reply 
+    reply = '{}'.format(reply_info)
+    # Post the message
+    response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, data=reply)
+    return 0
